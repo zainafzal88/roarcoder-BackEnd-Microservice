2a33e482f7cb16d5d984e71bf0bff38fc62a5776
Zain Afzal <zain.afzal@rise.cx>
2020-04-01 12:23:29 +1100

    Code review improvements and API path change in template.yaml

--- getuser.js
+++ getuser.js
@@ -1,26 +1,29 @@
-import { v4 as uuidv4 } from 'uuid';
 const AWS = require('aws-sdk')
 const dynamodb = new AWS.DynamoDB.DocumentClient({region: 'ap-southeast-2'})
 
 exports.lambdaHandler = async (event, context) => {
 
-    var params = {
-        TableName: process.env.TABLE,
-    };
-    
-    var getUserInfo = await dynamodb.scan(params).promise()
+    let params = {
+        TableName:process.env.TABLE,
+        Key: {
+            UUID: "1"
+        },
+        ProjectionExpression:"firstName, lastName, title, email, url_LinkedIn, url_Github"
+    }
+
+    let getUserInfo = await dynamodb.get(params).promise()
     let response;
         
     try {
         response = {
             statusCode: 200,
-            body: JSON.stringify(getUserInfo),
+            body: JSON.stringify(getUserInfo.Item, null, 3)
         };
     }
     catch (err) {
         response = {
             statusCode: 200,
-            body: "An error occured: " + err,
+            body: "An error occured: " + err
         };
     }
 
